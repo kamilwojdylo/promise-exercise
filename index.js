d2aac5793dfc074e30436fea1ed0ca801d230141
Kamil <kamilwojdylo@wp.pl>
2021-02-09 12:24:04 +0100

    Sequential execution.

--- index.js
+++ index.js
@@ -7,3 +7,43 @@ import {
   stats,
   storeFileContent,
 } from './utils.js';
+
+const urlToSpider = 'http://localhost:8080';
+const finalCb = () => {
+  console.log('Operation finished.');
+  console.log(stats);
+};
+
+spider(urlToSpider, 2, finalCb);
+
+function spider(link, nesting, doneCb) {
+  updateStats(link);
+  downloadFile(link, fileDownloaded);
+
+  function fileDownloaded(body, downloadedLink) {
+    storeFileContent(downloadedLink, body);
+
+    if (nesting === 0) {
+      return doneCb();
+    }
+
+    const linksOnPage = findOtherLinks(downloadedLink, body);
+    const linksCount = linksOnPage.length;
+
+    if (linksCount < 1) {
+      return doneCb();
+    }
+
+    function iterate(idx) {
+      if (idx === linksCount) {
+        return doneCb();
+      }
+      const nextLink = linksOnPage[idx];
+      spider(nextLink, nesting - 1, () => {
+        idx++;
+        iterate(idx);
+      });
+    }
+    iterate(0);
+  }
+}
---
+++ testServer.js
@@ -0,0 +1,85 @@
+import http from 'http';
+import url from 'url';
+
+const RESPONSE_TIME = 1000;
+
+const
+stats = {
+  third: 0,
+  second: 0,
+  first: 0
+};
+
+const server = http.createServer((req, res) => {
+  const responseBody = `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="UTF-8"/>
+    </head>
+    <body>
+      <h1>Hello World!</h1>
+
+      <p>This is a <a href="second/1/">link</a></p>
+      <p>This is a <a href="second/2/">link</a></p>
+      <p>This is a <a href="second/3/">link</a></p>
+      <p>This is a <a href="second/4/">link</a></p>
+      <p>This is a <a href="second/5/">link</a></p>
+      <p>This is a <a href="second/6/">link</a></p>
+
+      <p>This is an <a href="https://wwww.wp.pl">external link</a></p>
+    </body>
+  </html>
+  `;
+
+  const secondResponseBody = `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="UTF-8"/>
+    </head>
+    <body>
+      <p>This is a second subpage.</p>
+      <p>Second <a href="third/1">link to thrid page</a></p>
+      <p>Second <a href="third/2">link to thrid page</a></p>
+    </body>
+  </html>
+  `;
+
+  const thirdResponseBody = `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="UTF-8"/>
+    </head>
+    <body>
+      <p>This is a third subpage.</p>
+    </body>
+  </html>
+  `;
+
+
+  res.setHeader('content-type', 'text/html');
+  const requestUrl = url.parse(req.url);
+  if ((requestUrl.pathname || '').includes('/second/third')) {
+    stats.third += 1;
+    console.log(stats);
+    return setTimeout(() => res.end(thirdResponseBody), RESPONSE_TIME);
+  }
+  if ((requestUrl.pathname || '').includes('/third')) {
+    stats.third += 1;
+    console.log(stats);
+    return setTimeout(() => res.end(thirdResponseBody), RESPONSE_TIME);
+  }
+  if ((requestUrl.pathname || '').includes('/second')) {
+    stats.second += 1;
+    console.log(stats);
+    return setTimeout(() => res.end(secondResponseBody), RESPONSE_TIME);
+  }
+
+  stats.first += 1;
+  console.log(stats);
+  return res.end(responseBody);
+});
+
+server.listen(8080, () => console.log('Server listens'));
